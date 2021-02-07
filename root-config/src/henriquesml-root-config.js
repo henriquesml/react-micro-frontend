e91d3c07235bd132cf2560aec77fcc0f770f6e36
henriquesml <henrique_schmeller@hotmail.com>
2021-02-07 13:22:50 -0300

    adição de root config

---
+++ root-config/src/henriquesml-root-config.js
@@ -0,0 +1,26 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    System.import(
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: (location) => location.pathname === "/",
+});
+
+registerApplication({
+  name: "@henriquesml/react-single",
+  app: () => System.import("@henriquesml/react-single"),
+  activeWhen: (location) => location.pathname === "/react-single",
+});
+
+registerApplication({
+  name: "@henriquesml/react-multiples",
+  app: () => System.import("@henriquesml/react-multiples"),
+  activeWhen: (location) => location.pathname === "/react-multiples",
+});
+
+start({
+  urlRerouteOnly: true,
+});
